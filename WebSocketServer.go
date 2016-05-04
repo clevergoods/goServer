971b5a5b105b83858c978115beef320dddb93ae3
avlasov <clevergoods@gmail.com>
2016-05-04 22:02:22 +0300

    Check MD5 sum

--- WebSocketServer.go
+++ WebSocketServer.go
@@ -1,4 +1,4 @@
-package goServer
+package main
 
 import (
 	"fmt"
@@ -7,6 +7,8 @@ import (
 	"golang.org/x/net/websocket"
 	"encoding/json"
 	"os"
+	"crypto/md5"
+	"bytes"
 )
 
 type Message struct {
@@ -14,6 +16,8 @@ type Message struct {
 	Md5Sum [16]byte
 	Body   []byte
 }
+
+var f *os.File
 
 func wsRepeat(ws *websocket.Conn) {
 	var err error
@@ -23,6 +27,7 @@ func wsRepeat(ws *websocket.Conn) {
 	if err = websocket.Message.Receive(ws, &reply); err != nil {
 		fmt.Println(err)
 	}
+
 	if err = websocket.Message.Send(ws, "OK"); err != nil {
 		fmt.Println("Cannot send ws message")
 		log.Fatal(err)
@@ -32,31 +37,36 @@ func wsRepeat(ws *websocket.Conn) {
 
 	err = json.Unmarshal(reply, &msg)
 	check(err)
+	md5Sum := md5.Sum(msg.Body)
+	if (bytes.Equal(md5Sum[:], msg.Md5Sum[:])) {
+		writeFile(msg.Body)
+	}
+}
 
-	f, err := os.OpenFile("output.txt", os.O_APPEND | os.O_WRONLY, 0600)
-	check(err)
-
-	_, err = f.Write(msg.Body)
+func writeFile(message []byte) {
+	_, err := f.Write(message)
 	check(err)
 }
 
 func check(err error) {
 	if err != nil {
 		fmt.Println(err)
+		panic(err)
 	}
+}
+
+func openFile(fileName string) {
+	var err error
+	f, err = os.OpenFile(fileName, os.O_APPEND | os.O_WRONLY | os.O_CREATE, 0600)
+	check(err)
 }
 
 func main() {
-
-	f, err := os.Create("output.txt")
-	check(err)
+	openFile("output.txt")
 	defer f.Close()
 	http.Handle("/", websocket.Handler(wsRepeat))
-
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe", err)
 	}
-
 }
-
 
