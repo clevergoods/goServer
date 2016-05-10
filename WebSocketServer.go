90201ac89c3019f9692953a431202bff318657bc
avlasov <clevergoods@gmail.com>
2016-05-11 00:08:39 +0300

    Order packets by number

--- WebSocketServer.go
+++ WebSocketServer.go
@@ -18,11 +18,13 @@ type Message struct {
 }
 
 var f *os.File
+var c chan Message
+var counter int
+var m map[int][]byte
 
 func wsRepeat(ws *websocket.Conn) {
 	var err error
 	var reply []byte
-	var msg Message
 
 	if err = websocket.Message.Receive(ws, &reply); err != nil {
 		fmt.Println(err)
@@ -33,19 +35,57 @@ func wsRepeat(ws *websocket.Conn) {
 		log.Fatal(err)
 	}
 
-	fmt.Println(reply)
+	go checkMd5Sum(reply)
+	go writeFile()
+}
 
-	err = json.Unmarshal(reply, &msg)
-	check(err)
-	md5Sum := md5.Sum(msg.Body)
-	if (bytes.Equal(md5Sum[:], msg.Md5Sum[:])) {
-		writeFile(msg.Body)
+func writeFile() {
+	for s := range (c) {
+		if counter == s.ID {
+			msg := s.Body
+			_, err := f.Write(msg)
+			check(err)
+			fmt.Println("from chan", counter)
+			fok := true
+
+			counter++
+			for ; fok; {
+				msg, fok = m[counter]
+				if fok {
+					_, err := f.Write(msg)
+					check(err)
+					fmt.Println("write from m", counter)
+					delete(m, counter)
+					counter++
+				}
+			}
+
+		}else {
+			m[s.ID] = s.Body
+			fok := true
+			var msg []byte
+			for ; fok; {
+				msg, fok = m[counter]
+				if fok {
+					_, err := f.Write(msg)
+					check(err)
+					fmt.Println("from m", counter)
+					delete(m, counter)
+					counter++
+				}
+			}
+		}
 	}
 }
 
-func writeFile(message []byte) {
-	_, err := f.Write(message)
+func checkMd5Sum(reply []byte) {
+	var msg Message
+	err := json.Unmarshal(reply, &msg)
 	check(err)
+	md5Sum := md5.Sum(msg.Body)
+	if (bytes.Equal(md5Sum[:], msg.Md5Sum[:])) {
+		c <- msg
+	}
 }
 
 func check(err error) {
@@ -57,11 +97,15 @@ func check(err error) {
 
 func openFile(fileName string) {
 	var err error
+	counter = 0
 	f, err = os.OpenFile(fileName, os.O_APPEND | os.O_WRONLY | os.O_CREATE, 0600)
 	check(err)
 }
 
 func main() {
+	c = make(chan Message, 100)
+	m = make(map[int][]byte)
+	counter = 0
 	openFile("output.txt")
 	defer f.Close()
 	http.Handle("/", websocket.Handler(wsRepeat))
